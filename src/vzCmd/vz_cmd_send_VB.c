c206cc527aa60329624bb1d9a1fd6f78b6682895
verem@INTERNAL.M1STEREO.TV <verem@INTERNAL.M1STEREO.TV@ff2bb774-c509-0410-9e0c-fc5896ccfff9>
2009-01-25 17:10:49 +0000

    VB.NET support added

--- src/vzCmd/vz_cmd_send_VB.c
+++ src/vzCmd/vz_cmd_send_VB.c
@@ -88,3 +88,80 @@ VZCMDSEND_API long __stdcall vzCmdSendVB(char* host, long count, struct vzCmdSen
 
 	return r;
 };
+
+VZCMDSEND_API long __stdcall vzCmdSendVB2008(char* host, long count, LPSAFEARRAY FAR *ArrayData)
+{
+	int i, l, r = 0;
+	char** argv;
+	BSTR* data;
+#ifdef _DEBUG
+	FILE* f;
+	char error_log[1024] = "";
+#else
+	char* error_log = NULL;
+#endif /* _DEBUG */
+
+	/* init sockets */
+	if(0 == init_done)
+	{
+		vz_cmd_send_init();
+		init_done = 1;
+	};
+
+#ifdef _DEBUG
+	f = fopen("vz_cmd_send_VB2008.log", "wt");
+	if(NULL != f)
+	{
+		fprintf(f, "host: [%s]\n", host);
+		fprintf(f, "count: [%d]\n", count);
+	};
+#endif /* _DEBUG */
+
+	/* allocate data */
+	argv = (char**)malloc(count * sizeof(char*));
+	data = (BSTR*)(*ArrayData)->pvData;
+	for(i = 0; i < count; i++)
+	{
+		/* get width */
+		l = WideCharToMultiByte(CP_UTF8, 0, data[i], -1, NULL, 0,  NULL, NULL);
+
+#ifdef _DEBUG
+		if(NULL != f)
+		{
+			fprintf(f, "cmd[%d] lenght: [%d]\n", i, l);
+		};
+#endif /* _DEBUG */
+
+		/* allocate */
+		argv[i] = (char*)malloc(l + 1);
+
+		/* convert */
+		WideCharToMultiByte(CP_UTF8, 0, data[i], -1, argv[i], l,  NULL, NULL);
+
+#ifdef _DEBUG
+		if(NULL != f)
+		{
+			fprintf(f, "cmd[%d] data: [%s]\n", i, argv[i]);
+		};
+#endif /* _DEBUG */
+	};
+
+	/* call sender */
+	if(count)
+		r = vz_cmd_send_strlist_udp(host, argv, count, error_log);
+
+#ifdef _DEBUG
+	if(NULL != f)
+	{
+		fprintf(f, "r = %d, error_log=[%s]\n", r, error_log);
+		fclose(f);
+	};
+#endif /* _DEBUG */
+
+	/* free data */
+	for(i = 0; i < count; i++)
+		free(argv[i]);
+	free(argv);
+
+	return r;
+};
