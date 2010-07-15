119d4b00060fc1967023cf2d8a37d330f9d58ccf
verem@INTERNAL.M1STEREO.TV <verem@INTERNAL.M1STEREO.TV@ff2bb774-c509-0410-9e0c-fc5896ccfff9>
2010-07-15 09:25:57 +0000

    RLE TAG support added

--- src/vz/vzImageLoadTGA.h
+++ src/vz/vzImageLoadTGA.h
@@ -34,9 +34,14 @@ static int vzImageLoadTGA(vzImage** pimg, char* filename)
     /* "Image Type - Field 3 (1 byte)" */
     int tga_image_type = fio_get_byte(f);
 
-    /* we supports only "Uncompressed, True-color Image" 
-        or "Uncompressed, Black-and-white Image" */
-    if((2 != tga_image_type) && (3 != tga_image_type))
+    /* check type supported */
+    if
+    (
+        (2  != tga_image_type) &&   /* uncompressed, true-color image */
+        (3  != tga_image_type) &&   /* uncompressed, black-and-white image */
+        (10 != tga_image_type) &&   /* run-length encoded, true-color image */
+        (11 != tga_image_type)      /*run-length encoded, black-and-white Image */
+    )
     {
         fclose(f);
         return -2;
@@ -85,7 +90,8 @@ static int vzImageLoadTGA(vzImage** pimg, char* filename)
     fio_skip(f, tga_id_length);
 
     /* check if image data is not outside file */
-    if(ftell(f) + tga_image_width * tga_image_height * (tga_pixel_depth >> 3) > tga_file_size)
+    if((ftell(f) + tga_image_width * tga_image_height * (tga_pixel_depth >> 3) > tga_file_size)
+        && 2 == tga_image_type && 3 == tga_image_type)
     {
         fclose(f);
         return -2;
@@ -101,15 +107,71 @@ static int vzImageLoadTGA(vzImage** pimg, char* filename)
     };
 
     /* load data */
-    for(int j, i = 0; i < tga_image_height; i++)
+    if(2 == tga_image_type || 3 == tga_image_type)
     {
+        for(int j, i = 0; i < tga_image_height; i++)
+        {
+            /* bit 5 is for top-t-obottom ordering: bottom(0) top(1) */
+            if(tga_image_descriptor & (1 << 5))
+                j = i;
+            else
+                j = tga_image_height - 1 - i;
+
+            fio_get_buffer(f, img->lines_ptr[j], tga_image_width * img->bpp);
+        };
+    }
+    else if(10 == tga_image_type || 11 == tga_image_type)
+    {
+        int r_cnt;
+        int j, jc, i = 0;
+        int linesize = tga_image_width * img->bpp;
+        unsigned char *buf_dst;
+
         /* bit 5 is for top-t-obottom ordering: bottom(0) top(1) */
         if(tga_image_descriptor & (1 << 5))
-            j = i;
+        {
+            j = 0;
+            jc = 1;
+        }
         else
-            j = tga_image_height - 1 - i;
+        {
+            j = tga_image_height - 1;
+            jc = -1;
+        };
 
-        fio_get_buffer(f, img->lines_ptr[j], tga_image_width * img->bpp);
+        while(!feof(f))
+        {
+            /* get Repetition Count field */
+            r_cnt = fio_get_byte(f);
+
+            buf_dst = (unsigned char*)img->lines_ptr[j] + i;
+
+            if(0x80 & r_cnt)
+            {
+                /* packet is a Run-length Packet */
+                fio_get_buffer(f, buf_dst, img->bpp);
+                for(r_cnt = r_cnt & 0x7F, i += img->bpp; r_cnt > 0; r_cnt--, i += img->bpp)
+                    memcpy((unsigned char*)img->lines_ptr[j] + i, buf_dst, img->bpp);
+            }
+            else
+            {
+                /* packet is a Raw Packet */
+                r_cnt &= 0x7F; r_cnt += 1; r_cnt *= img->bpp;
+                fio_get_buffer(f, buf_dst, r_cnt);
+                i += r_cnt;
+            };
+
+            /* check to switch to new scanline */
+            if(i >= linesize)
+            {
+                i = 0;
+                j += jc;
+
+                /* check for pic end */
+                if(j < 0 || j >= tga_image_height)
+                    break;
+            };
+        };
     };
 
     /* close file */
